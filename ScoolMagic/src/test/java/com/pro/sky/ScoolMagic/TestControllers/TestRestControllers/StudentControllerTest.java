c9b53d0ce5521922ef6fe8b27d1d4a4928d4fad5
AlexDv <DvoinikovAV@yandex.ru>
2024-09-11 13:25:07 +0300

    Сделано тестирование факультета

--- ScoolMagic/src/test/java/com/pro/sky/ScoolMagic/TestControllers/TestRestControllers/StudentControllerTest.java
+++ ScoolMagic/src/test/java/com/pro/sky/ScoolMagic/TestControllers/TestRestControllers/StudentControllerTest.java
@@ -1,10 +1,30 @@
 package com.pro.sky.ScoolMagic.TestControllers.TestRestControllers;
 
+import com.pro.sky.ScoolMagic.Controllers.FacultyController;
+import com.pro.sky.ScoolMagic.Controllers.StudentController;
+import com.pro.sky.ScoolMagic.Services.StudentServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class StudentControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private StudentController studentController;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+    @Autowired
+    private StudentServiceImpl studentService;
+
 
     @Test
     void createStudent() {
